396cb391d4b9ecf83bd487bf4632a77565d635c2
Max Matt <77360455+Vsemhoy@users.noreply.github.com>
2025-06-06 01:22:27 +0300

    up

--- src/Modules/MIMITRACK_25/TASKAPP/TASKLIST_25/components/style/TaskListRowCard.jsx
+++ src/Modules/MIMITRACK_25/TASKAPP/TASKLIST_25/components/style/TaskListRowCard.jsx
@@ -1,28 +1,90 @@
-import { EnterOutlined } from '@ant-design/icons';
+import { BarsOutlined, EnterOutlined } from '@ant-design/icons';
+import { Button, Dropdown, Input } from 'antd';
 import React, { useEffect, useState } from 'react';
+import InputContentEditable from '../../../../../../Components/MimiTemplate/components/CONTENTEDITABLE/InputContentEditable';
 
+
+const menuItems = [
+{
+    key: '3',
+    label: (
+      <div>
+        Добавить секцию
+      </div>
+    ),
+  },
+  {
+    key: '1',
+    label: (
+      <div>
+        Добавить подсекцию
+      </div>
+    ),
+  },
+  {
+    key: '2',
+    label: (
+      <div>
+        Добавить задачу
+      </div>
+    ),
+  },
+  {
+    key: '3',
+    label: (
+      <div>
+        Добавить подзадачу
+      </div>
+    ),
+  },
+];
 
 const TaskListRowCard = (props) => {
 
     const [item, setItem] = useState(props.data);
     const [level, setLevel] = useState(props.level ? props.level : 0);
 
+    const [itemTitle, setItemTitle] = useState(props.data?.title);
+
+    const changeTitleText = (value) => {
+        // console.log('ev', ev)
+        setItemTitle(value);
+    }
+
   return (
     <div className={`mi-flat-task-wrapper mi-level-${level}`}>
-        <div className={'mi-flat-task'}>
-            <div className={'mi-flat-task-hierarchy'}>
-                {/* <div style={{transform: 'scale(-1, 1)'}}><EnterOutlined /></div> */}
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+        <div className={`mi-flat-task  ${item.type === 'section' ? 'mi-ft-section-item' : ''}`}>
+            <div className={'mi-flat-task-root'}>
+                {item.type === 'section' ? '📁' : '📄'}
             </div>
-            <div className={'mi-flat-task-content'}>
-                {item.title}
+            <div className={'mi-flat-task-body'}>
+                <div className={'mi-flat-task-first-stage'}>
+                    <div className={'mi-flat-task-content'}>
+        
+                        <InputContentEditable
+                            value={itemTitle}
+                            onChange={changeTitleText}
+                            />
+                    </div>
+                </div>
+                <div className={'mi-flat-task-second-stage mi-flex-space'}>
+                    <div>
+                        
+                    </div>
+                     <div className={'mi-flat-task-options'}>
+                        <div className={'mi-flat-task-control-box'}>
+                            <Dropdown menu={{ items:  menuItems }} placement="topRight" arrow>
+                                <Button
+                                    type={'text'}
+                                    size={'small'}><BarsOutlined /></Button>
+                            </Dropdown>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className={'mi-flat-task-options'}>
-                OHOHO
-            </div>
+
+            
+           
         </div>
         {item?.children?.length > 0 && (
             <div className={'mi-flat-task-childrens'}>
